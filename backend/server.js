0a89276a7d63f70c4103cc6cc8939d6c9a092c15
Shubham Pal <shubhampal31102003@gmail.com>
2025-04-13 21:14:51 +0530

    static file removed

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,13 @@ app.use(cors({ origin: 'https://myproduct-store.netlify.app' }));
 
 app.use('/api/products', productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+//   });
+// }
 
 app.listen(PORT, () => {
   connectDB();
